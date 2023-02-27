57317a733770392b91453444ecbdb8a5b642a7ce
maqingbo <wmaqingbo@163.com>
2023-02-27 18:14:45 +0800

    feat: 添加英文版本（未完成）；

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,10 +8,24 @@ module.exports = {
   ],
   themeConfig: {
     // displayAllHeaders: true,
+    nav: [
+      { text: '中文', link: '/version_1/01.md' },
+      { text: '英文', link: '/version_2/01.md' }
+    ],
     lastUpdated: '上次更新',
     sidebarDepth: 2,
     sidebar: {
-      '/chapters/': [
+      '/version_1/': [
+        '01.md', '02.md', '03.md', '04.md',
+        '05.md', '06.md', '07.md', '08.md',
+        '09.md', '10.md', '11.md', '12.md',
+        '13.md', '14.md', '15.md', '16.md',
+        '17.md', '18.md', '19.md', '20.md',
+        '21.md', '22.md', '23.md', '24.md',
+        '25.md', '26.md', '27.md', '28.md',
+        '29.md', '30.md', '31.md', '32.md'
+      ],
+      '/version_2/': [
         '01.md', '02.md', '03.md', '04.md',
         '05.md', '06.md', '07.md', '08.md',
         '09.md', '10.md', '11.md', '12.md',
--- docs/README.md
+++ docs/README.md
@@ -4,6 +4,6 @@ heroImage: /images/cover.jpg
 heroText: 我们赖以生存的隐喻
 tagline: '乔治·莱考夫 / 马克·约翰逊 [著] - 何文忠 [译]'
 actionText: 开始阅读
-actionLink: /chapters/01/
+actionLink: /version_1/01/
 # footer: MIT Licensed | Copyright © 2018-present 上野千鹤子
 ---
